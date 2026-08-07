07829aab08fa5687ca81037cec6daa9956237eea
karkera-saakshi <saakshikarkera81@gmail.com>
2026-08-07 13:43:01 +0530

    Added updated origin

--- backend/server.js
+++ backend/server.js
@@ -6,7 +6,7 @@ const cookieParser = require("cookie-parser");
 const studentRoute = require("./routes/studentRoute"); 
 
 let app = express();
-app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+app.use(cors({ origin: "https://anara-skills-foundation-student-99qk.onrender.com", credentials: true }));
 app.use(express.json());
 app.use(cookieParser());
 app.use("/", studentRoute);
