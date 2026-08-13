f4523e41665a1e13a24ae7651be79b3b69ef3fcc
karkera-saakshi <saakshikarkera81@gmail.com>
2026-08-13 08:19:52 +0530

    changed url of add student page and logout page

--- frontend/src/components/Sidebar.jsx
+++ frontend/src/components/Sidebar.jsx
@@ -11,7 +11,7 @@ function Sidebar() {
   };
 
   let handleLogout = () => {
-    axios.post("https://anara-skills-foundation-student-reg-sigma.vercel.app/api/logout", {}, { withCredentials: true })
+    axios.post("/api/logout", {}, { withCredentials: true })
       .then(() => {
         navigate('/login');
       })
--- frontend/src/pages/AddStudent.jsx
+++ frontend/src/pages/AddStudent.jsx
@@ -17,7 +17,7 @@ function AddStudent() {
 
   let handleSubmit = (e) => {
     e.preventDefault();
-    axios.post("https://anara-skills-foundation-student-reg-sigma.vercel.app/api/create", student)
+    axios.post("/api/create", student)
       .then(() => {
         alert("Added Details successfully!");
         setStudent({
