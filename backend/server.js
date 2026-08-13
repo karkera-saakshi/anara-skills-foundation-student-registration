1a8b91e5b78289d5d0c1c944ab6080e13a99e989
karkera-saakshi <saakshikarkera81@gmail.com>
2026-08-13 07:03:32 +0530

    added api folder

--- backend/server.js
+++ backend/server.js
@@ -1,22 +1,23 @@
-require("dotenv").config(); 
-
+require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const cookieParser = require("cookie-parser");
-const studentRoute = require("./routes/studentRoute"); 
+const studentRoute = require("./routes/studentRoute");
 
 let app = express();
 app.use(cors({
     origin: [
         'https://anara-skills-foundation-student-reg-sigma.vercel.app',
-        'http://localhost:5173' 
+        'http://localhost:5173'
     ],
-    credentials: true 
+    credentials: true
 }));
 app.use(express.json());
 app.use(cookieParser());
 app.use("/api", studentRoute);
 
+if (require.main === module) {
+  app.listen(9000, () => console.log("I am listening"));
+}
 
-app.listen(9000, () => console.log("I am listening"));
 module.exports = app;
